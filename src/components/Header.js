f19a1d8a0084a0c1bacc307be9ebb69798a54da9
SinaSNP <ssasanpour99@gmail.com>
2025-04-08 22:02:35 +0300

    header partially fixed for narrow

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,21 @@ import { faGithub, faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-i
 
 export default function Header({ dark, toggleDark }) {
   return (
-    <header className="p-6 flex justify-between items-center bg-teal-500 dark:bg-[#17313c]">
-      <div className="flex items-center gap-4">
-        <img src="/img/image2.jpg" alt="Profile" className="w-16 h-16 rounded-full border-2 border-white dark:border-black" />
+    <header className="p-6 flex flex-col xs:items-center xs:text-center xs:gap-6 md:flex-row md:justify-between md:items-center bg-teal-500 dark:bg-[#17313c]">
+      <div className="flex flex-col sm:flex-row sm:items-center gap-4 text-center sm:text-left">
+        <img src="/img/profile.jpg" alt="Profile" className="w-16 h-16 rounded-full border-2 border-white dark:border-black md:items-center" />
         <div>
           <h1 className="text-2xl font-bold">Sina Sasanpour </h1>
           <p className="text-sm">Junior Developer | Assossiate Degree from UÜ</p>
-          <div className="flex gap-3 mt-1">
-            <a href="https://github.com/RealSinaSnp" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-200">
-            <FontAwesomeIcon icon={faGithub} size={18}  color={dark ? "white": "black"} />
+          <div className="flex gap-3 mt-1 md:items-center">
+            <a href="https://github.com/RealSinaSnp" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-900">
+            <FontAwesomeIcon icon={faGithub} size={18}  color={dark ? "#98FF98": "#17313c"} />
             </a>
             <a href="https://www.linkedin.com/in/realsinasnp/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-200">
-            <FontAwesomeIcon icon={faLinkedin} size={18}  color={dark ? "white": "black"} />
+            <FontAwesomeIcon icon={faLinkedin} size={18}  color={dark ? "#98FF98": "#17313c"} />
             </a>
             <a href="https://x.com/RealSinaSNP" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-200">
-              <FontAwesomeIcon icon={faXTwitter} size={18}  color={dark ? "white": "black"} />
+              <FontAwesomeIcon icon={faXTwitter} size={18}  color={dark ? "#98FF98": "#17313c"} />
             </a>
             
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': {'max': '690px'}
+      },
+    },
   },
   plugins: [],
 };
