839f384ff1d29c0df4b8614f2567947beb3f26ca
SinaSNP <ssasanpour99@gmail.com>
2025-06-22 23:25:45 +0300

    footer dark mode

--- components/FooterMain.tsx
+++ components/FooterMain.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row gap-5 py-10  max-w-7xl mx-auto items-start justify-between">
         {/* Left Column */}
         <div className="flex-1 text-center md:text-left">
-          <h2 className="text-lg font-semibold text-white dark:text-black">Sina Sasanpour</h2>
+          <h2 className={`text-lg font-semibold ${isDark ? "text-white":"text-black"}`}>Sina Sasanpour</h2>
           <p className="mt-2 text-sm">© 2025 | All rights reserved.</p>
         </div>
 
@@ -21,7 +21,7 @@ const Footer = () => {
         <div className="md:grid  md:grid-cols-2 gap-8 text-center md:text-left ml-auto -right-5">
           {/* Skills */}
           <div>
-            <h3 className="text-lg font-semibold text-white dark:text-black">My Websites</h3>
+            <h3 className={`text-lg font-semibold ${isDark ? "text-white":"text-black"}`}>My Websites</h3>
             <ul className="mt-2 space-y-2 text-sm">
               <li>
                 <a href="https://sinasnp.com/blog" target="_blank" rel="noopener noreferrer" className="hover:text-teal-400 transition-colors">Weblog</a>
@@ -35,7 +35,7 @@ const Footer = () => {
 
           {/* Contact */}
           <div>
-            <h3 className="text-lg font-semibold text-white dark:text-black">Contact</h3>
+            <h3 className={`text-lg font-semibold ${isDark ? "text-white":"text-black"}`}>Contact</h3>
             <ul className="mt-2 space-y-2 text-sm">
               <li>
                 <a
